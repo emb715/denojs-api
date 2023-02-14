c5511634a9b024e311e7374477452a5edd32f713
Ezequiel Benitez <ezequiel.benitez@agileengine.com>
2023-02-14 16:01:33 -0300

    add prisma to the project looking for a ORM more stable

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@ import "https://deno.land/std@0.177.0/dotenv/load.ts";
 
 const __DEV__ = Deno.env.get("NODE_ENV") !== "production" ?? false as boolean;
 
+const DATABASE_URL = Deno.env.get("DATABASE_URL") ?? "" as string;
+
 const MONGO_DB_CONNECT = Deno.env.get("MONGO_DB_CONNECT") ?? "" as string;
 const MONGO_DB_URL = Deno.env.get("MONGO_DB_URL") ?? "" as string;
 const MONGO_DB_NAME = Deno.env.get("MONGO_DB_NAME") ?? "" as string;
@@ -18,6 +20,7 @@ export {
   API_PORT,
   API_URL,
   API_VERSION,
+  DATABASE_URL,
   MONGO_DB_CONNECT,
   MONGO_DB_NAME,
   MONGO_DB_PASS,
